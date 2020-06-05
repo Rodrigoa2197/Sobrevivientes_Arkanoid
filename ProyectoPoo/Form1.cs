f1e936c0751ce3a5b3bff948fad2c7265e496a5f
SrOrellana <00163618@uca.edu.sv>
2020-06-05 16:25:54 -0600

    Update connection

--- ProyectoPoo/Form1.cs
+++ ProyectoPoo/Form1.cs
@@ -31,5 +31,10 @@ namespace ProyectoPoo
             frame.ShowDialog();
             
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
