90e0be58ef6c9160acc54c9dee63a659ad395792
Jose Nasser <00245818@uca.edu.sv>
2020-06-18 16:01:03 -0600

    Fixing lifes Exception

--- ProyectoPoo/Model/Connection.cs
+++ ProyectoPoo/Model/Connection.cs
@@ -8,7 +8,7 @@ namespace ProyectoPoo
         private static string host = "127.0.0.1",
             database = "ProyectoPoo",
             userId = "postgres",
-            password = "sebas";
+            password = "gaseosa1234";
         
             private static string sConnection =
             $"Server={host};Port=5432;User Id={userId};Password={password};Database={database};";
